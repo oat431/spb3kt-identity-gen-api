f886a8fc5ba9b32634f3df068ef799fb0ba72cc6
oat431 <oat431@gmail.com>
2025-11-18 23:25:18 +0700

    feat: add gen random identity

--- src/main/kotlin/panomete/project/spb3kotlin/payload/response/IdentityDTO.kt
+++ src/main/kotlin/panomete/project/spb3kotlin/payload/response/IdentityDTO.kt
@@ -1,13 +1,14 @@
 package panomete.project.spb3kotlin.payload.response
 
 import io.swagger.v3.oas.annotations.media.Schema
+import panomete.project.spb3kotlin.entity.Name
 import java.time.LocalDateTime
 
 @Schema(description = "Identity Response")
 data class IdentityDTO(
     val nationalID: String,
     val title : String,
-    val firstName: String,
-    val lastName: String,
+    val firstName: Name,
+    val lastName: Name,
     val birthDate: LocalDateTime,
 )
